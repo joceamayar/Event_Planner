a20974603845fdba9d491c367e3e02a8489e5dea
Simon Magidenko <smagidenko@gmail.com>
2023-09-06 10:35:44 -0400

    Folder Structuring

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const projectRoutes = require('./eventRoutes');
+const landingPageRoutes = require('./landingPageRoutes')
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use('/landingPage', landingPageRoutes);
 
 module.exports = router;
