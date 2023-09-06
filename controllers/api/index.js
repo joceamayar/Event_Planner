e026ac79e044f2b024f81fb4372856b1cd753e97
joceamayar <jocelin@tavlee.com>
2023-09-06 13:04:59 -0400

    Organization/eventroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./eventRoutes');
+const eventRoutes = require('./eventRoutes');
+const categoriesRoutes = require('./organizationRoutes');
 const landingPageRoutes = require('./landingPageRoutes')
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/events', eventRoutes);
+router.use('/categories', categoriesRoutes);
 router.use('/landingPage', landingPageRoutes);
 
 module.exports = router;
