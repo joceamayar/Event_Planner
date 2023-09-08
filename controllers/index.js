d27051c1dca611aa612881799b74bebbfe6dc1a4
joceamayar <jocelin@tavlee.com>
2023-09-07 21:53:02 -0400

    ticketmaster api is working and categories, events  are displaying, seeded classifications

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api/index');
 const homeRoutes = require('./homeRoutes');
+const eventRoutes = require('./eventRoutes');
 
 router.use('/', homeRoutes);
+router.use('/event', eventRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
---
+++ public/js/categories.js
@@ -0,0 +1,1 @@
+let categories = document.getElementById('');
