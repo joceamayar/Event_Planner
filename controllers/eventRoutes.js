de9a734f0f3794aff17979ded89c316af407dac7
[Keval Patel] <[khp90611@gmail.com]>
2023-09-10 17:51:14 -0400

    Pushing up to get help from ask BCS

--- controllers/eventRoutes.js
+++ controllers/eventRoutes.js
@@ -69,113 +69,119 @@ router.get('/', async (req, res) => {
 ///-------------------------Eventpage Route-----------------------------///
 // Needed a new route for my render page. Instead of putting a bunch of if statements in the above route, this would be cleaner. 
 // localhost:3001/event/get?eventID=Z7r9jZ1AdqwP_&classification=..& >>>This is to include both eventID and classification ID in the parameters
-router.get(`/:id`, async (req,res)=> {
+router.get(`/:id`, async (req, res) => {
   // get route parameters (see above example route)
-  
+
   //Just setting the class to Miscellaneous key 
   let classification_id = req.query.classification_id
 
   //Setting category equal to "Random"
   let category = "Random"
-  let photoID;
   //Created an array of all the classifications and their associated IDs
   let categoryArr = [
     {
       cat: "Miscellaneous",
-      key: "KZFzniwnSyZfZ7v7n1",
+      key: "1",
       imgKey: "34Xicn82lY4"
     },
     {
       cat: "Sports",
-      key: "KZFzniwnSyZfZ7v7nE",
+      key: "2",
       imgKey: "0B_S1vTY0NU"
     },
     {
       cat: "Music",
-      key: "KZFzniwnSyZfZ7v7nJ",
+      key: "3",
       imgKey: "aWXVxy8BSzc"
     },
     {
       cat: "Arts & Theatre",
-      key: "KZFzniwnSyZfZ7v7na",
+      key: "4",
       imgKey: "gIDMmhKARLk"
     },
     {
       cat: "Undefined",
-      key: "KZFzniwnSyZfZ7v7nl",
+      key: "5",
       imgKey: "yueLIYXDpzw"
     },
     {
       cat: "Film",
-      key: "KZFzniwnSyZfZ7v7nn",
+      key: "6",
       imgKey: "nLl5sJnElxY"
     }
-]
+  ]
 
-    //Finding the category where the classfication_id from the query parameters is equal to the classification key
-    let foundCategory = await categoryArr.find(category => category.key===classification_id);
-    console.log(foundCategory)
-    
-    //If we find a matching value then get the category name associated with that key
-    if(foundCategory){
-      category = foundCategory.cat
-      photoID = foundCategory.imgKey
+  //Finding the category where the classfication_id from the query parameters is equal to the classification key
+
+    let foundCategory = await categoryArr.find(category => category.key == classification_id);
+
+  //If we find a matching value then get the category name associated with that key and if not then set the category = "Random"
+  if (foundCategory) {
+    category = foundCategory.cat
+  }
+  else {
+    console.log("Category not found and category Image not found")
+    category = "Random"
+  }
+
+  //---------------------Fetch for Category Banner Image -----------------//
+
+  let unsplashKEY = "Ftv1Z09dCkfC4h_vSuAWUHQL1PRguPeLoejKjjc-1sQ"
+  let photoID = foundCategory.imgKey
+  //If the photoID is undefined then set it to the miscellaneous
+  if (photoID == undefined) {
+    photoID = "34Xicn82lY4"
+  };
+  console.log(photoID)
+  let bannerURL;
+
+  let getImageData = async (photoID, unsplashKEY) => {
+    let allImageData = await fetch(`https://api.unsplash.com/photos/${photoID}/?client_id=${unsplashKEY}`, {
+      method: "GET"
+    })
+    //If the response is okay then get the URL's. If not then try a new KEY
+    if (allImageData.ok) {
+      let bannerImageData = await allImageData.json()
+      bannerURL = bannerImageData.urls.small
     }
-    else{
-      console.log("Category not found and category Image not found")
+    else if (!allImageData.ok) {
+      unsplashKEY = "3ToKaeZv1WWRFpRIRC6wrqtP0uSlaL4mP1_mjCAlGGw"
+      await getImageData(photoID, unsplashKEY);
     }
-    
-    //---------------------Fetch for Category Banner Image -----------------//
-    
-    let unsplashKEY = "Ftv1Z09dCkfC4h_vSuAWUHQL1PRguPeLoejKjjc-1sQ"
-    let bannerURL;
-    
-    let getImageData = async(photoID, unsplashKEY) =>{
-      let allImageData = await fetch(`https://api.unsplash.com/photos/${photoID}/?client_id=${unsplashKEY}`, {
-        method: "GET"})
-        //If the response is okay then get the URL's. If not then try a new KEY
-        if (allImageData.ok) {
-          let bannerImageData = await allImageData.json()
-          bannerURL = bannerImageData.urls.small
-        }
-        else if(!allImageData.ok){
-          unsplashKEY = "3ToKaeZv1WWRFpRIRC6wrqtP0uSlaL4mP1_mjCAlGGw"
-          getImageData(photoID, unsplashKEY);
-        }
-      }
-      
-      getImageData(photoID, unsplashKEY)
-      
-      
-      //----------------Fetch for Ticketmaster Data---------------//
-      //let event_id = req.params.id;
-      let event_id = req.params.id;
-      //API key for ticketmaster
-      const apiKey = "ooGU8uX0cAG4SM9WQPPlO5iFhuOfdLN2";
-      
-      //Setting the url
-      let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}`;
-      
-      //If there is an eventID in the parameters then set the url to the one below
-      if(event_id) {
-        url = `https://app.ticketmaster.com/discovery/v2/events/${event_id}.json?apikey=${apiKey}`;
-  } 
-  else{
+  }
+
+  await getImageData(photoID, unsplashKEY)
+
+
+  //----------------Fetch for Ticketmaster Data---------------//
+  //let event_id = req.params.id;
+  let event_id = req.params.id;
+  //API key for ticketmaster
+  const apiKey = "ooGU8uX0cAG4SM9WQPPlO5iFhuOfdLN2";
+
+  //Setting the url
+  let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}`;
+
+  //If there is an eventID in the parameters then set the url to the one below
+  if (event_id) {
+    url = `https://app.ticketmaster.com/discovery/v2/events/${event_id}.json?apikey=${apiKey}`;
+  }
+  else {
     //Renders wrong route if the eventID is not included in the query parameters//
     res.send("<h1>Wrong Route!</h1>")
   }
-  
+
   //Fetching the data from API for the event
   let response = await fetch(url, {
     method: 'GET'
   });
-  
+
   let data = await response.json()
-  
+
   //Using dayjs to set the format of the date to the Spelled out Month, Two-digit day, Four-digit year
   let dateFormat = dayjs(data.dates.start.localDate).format('MMMM DD, YYYY')
-  
-  let renderData = {      
+
+  let renderData = {
     //Using the classification id to render the right category name
     classification: category,
     categoryImg: bannerURL,
@@ -187,17 +193,18 @@ router.get(`/:id`, async (req,res)=> {
     eventID: data.id,
     ticketmaster_url: data.url,
     //Gets first 5 of postal code because it can be more than 5 digits>> For code below//
-    zip_code: data._embedded.venues[0].postalCode.slice(0,5),
+    zip_code: data._embedded.venues[0].postalCode.slice(0, 5),
     //The ? states that if there is NO data and NO priceRanges and NO min/max  in the API info for that event then set that value equal to "N/A" >> Called a ternary operator
     price: {
       min: data?.priceRanges?.min ? data?.priceRanges?.min : "N/A",
-      max: data?.priceRanges?.max ? data?.priceRanges?.min : "N/A"
-    }, 
+      max: data?.priceRanges?.max ? data?.priceRanges?.max : "N/A"
+    },
     classification_id: data.classifications[0].segment.id,
     venue: data._embedded.venues[0].name,
-    imageURL: data.images.find(image => image.ratio==="4_3").url,
+    imageURL: data.images.find(image => image.ratio === "4_3").url,
     //The ? states that if there is NO data and NO seatmap and NO static curl  in the API info for that event then set that value equal to "Check Ticketmaster for a SeatMAP >> Called a ternary operator"
-    seatMap: data?.seatmap?.staticurl ?  data?.seatmap?.staticurl : "Check Ticketmaster for a SeatMap"}
+    seatMap: data?.seatmap?.staticurl ? data?.seatmap?.staticurl : "Check Ticketmaster for a SeatMap"
+  }
 
 
 
