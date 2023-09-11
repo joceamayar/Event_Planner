1399ef0f5d81c50a55a3fe0b60888669673bcdf4
joceamayar <jocelin@tavlee.com>
2023-09-11 03:12:17 -0400

    tailwind styles

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,22 @@
 let fetchOnPageLoad = async () => {
-    try {
-        const response = await fetch('/api/classifications', {
-            method: 'GET'
-        })
+    // try {
+    //     const response = await fetch('/api/classifications', {
+    //         method: 'GET'
+    //     })
 
-        if (response.ok) {
-            // If successful, redirect the browser to the profile page
-            let data = await response.json()
-            window.classifications = data;
-            console.log(window.classifications)
-            return;
-        } else {
-            alert(response.statusText);
-        }
+    //     if (response.ok) {
+    //         // If successful, redirect the browser to the profile page
+    //         let data = await response.json()
+    //         window.classifications = data;
+    //         console.log(window.classifications)
+    //         return;
+    //     } else {
+    //         alert(response.statusText);
+    //     }
 
-    } catch (error) {
-        console.log(error);
-    }
+    // } catch (error) {
+    //     console.log(error);
+    // }
 }
 
 fetchOnPageLoad();
-
