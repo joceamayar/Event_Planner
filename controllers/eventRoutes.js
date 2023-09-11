bd4b7ce804d266bc97f2495c9adcaf92ce1b9777
Simon Magidenko <smagidenko@gmail.com>
2023-09-10 22:15:43 -0400

    added styling to categories page

--- controllers/eventRoutes.js
+++ controllers/eventRoutes.js
@@ -1,28 +1,32 @@
 const { Classification } = require('../models');
-
 const router = require('express').Router();
 
-// MVC controller
-// This is not an API route - this route returns the HTML page with Event data already fetched, and replaces the handlebars template.
-// Example route: http://localhost:3001/event?classification_id=2&ticketmaster_classification_id=KZFzniwnSyZfZ7v7n1&zip_code=02169
-// ^ this is a browser url, not an API route - Pass in classification_id, ticketmaster_classification_id and zip_code through the browser url
-//** MVC controller
-// Example route: http://localhost:3001/event?classification_id=1&zip_code=02169
-router.get('/', async (req, res) => {
+// Wrapper function to handle routes
+async function handleRoutes(req, res) {
+  // Dynamically import 'node-fetch' as an ESM module
+  const { default: fetch } = await import('node-fetch');
+
+  // Function to format a date as MM/DD/YYYY
+  function formatDate(date) {
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+    return `${month}/${day}/${year}`;
+  }
 
   // get route parameters (see above example route)
   let zip_code = req.query.zip_code;
 
-  //get classification from database
+  // get classification from the database
   let classification_id = req.query.classification_id;
-  const classification = await Classification.findByPk(classification_id)
+  const classification = await Classification.findByPk(classification_id);
   const classification_name = classification.classification_name;
 
-  // get events dynamically from ticket master
+  // get events dynamically from Ticketmaster
   const apiKey = "ooGU8uX0cAG4SM9WQPPlO5iFhuOfdLN2";
   let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}`;
 
-  // add ticket master classification id stored from database
+  // add Ticketmaster classification id stored from the database
   if (classification.ticketmaster_classification_id) {
     url += `&classificationId=${classification.ticketmaster_classification_id}`;
   }
@@ -32,36 +36,41 @@ router.get('/', async (req, res) => {
     url += `&postalCode=${zip_code}`;
   }
 
-  //add more
-
-  // fetch data from ticket master
+  // Fetch data from Ticketmaster
   let response = await fetch(url, {
     method: 'GET'
   });
 
   let data = await response.json()
   if (data._embedded && data._embedded.events) {
-
     let events = [];
 
     let ticketMasterEvents = data._embedded.events;
     for (let i = 0; i < ticketMasterEvents.length; i++) {
       const event = ticketMasterEvents[i];
 
+      // Format the start_date using the formatDate function
+      const formattedStartDate = formatDate(new Date(event.dates.start.dateTime));
+
       events.push({
         classification_id: classification_id,
         id: event.id,
         name: event.name,
         imageUrl: event.images[0].url,
-        start_date: event.dates.start.dateTime
+        start_date: formattedStartDate,
       });
     }
 
-    // giving back html (This gets the data to render handlebars)
-    res.render('event', { classification_name, events })
+    // Render the Handlebars template with the formatted data
+    res.render('event', { classification_name, events });
   } else {
-    res.render('event', { classification_name })
+    res.render('event', { classification_name });
   }
+}
+
+// Route handler that calls the wrapper function
+router.get('/', async (req, res) => {
+  await handleRoutes(req, res);
 });
 
 module.exports = router;
