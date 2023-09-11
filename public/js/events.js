c09e0d1775cb6f5677196e68f1e598d941f18f51
joceamayar <jocelin@tavlee.com>
2023-09-10 22:16:58 -0400

    fix systax errors on search by zipcode

--- public/js/events.js
+++ public/js/events.js
@@ -1,6 +1,6 @@
 let zipCodeButton = document.querySelector('#search-zip')
 if (zipCodeButton) {
-    let classificationId = zipCodeButton.getAttribute('data-zip');
+    let classificationId = zipCodeButton.getAttribute('data-classification');
     zipCodeButton.addEventListener('click', () => {
         let zipCodeInput = document.querySelector('#zip_code_input')
         if (zipCodeInput.value) {
